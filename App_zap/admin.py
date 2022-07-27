d9d80f0bf42a29c91480c3d6c51fd05d88eb43ce
Vicente <vicente_consoli@hotmail.com>
2022-07-27 00:12:13 -0500

    se agregaron meta datos a todas las clases

--- App_zap/admin.py
+++ App_zap/admin.py
@@ -6,9 +6,25 @@ from App_zap.models import Accesorios, Proveedores, Sucursales, Zapatos
 
 class ZapatoAdmin(admin.ModelAdmin):
     list_display = ['modelo', 'talla', "color", "precio"]
+    search_fields = ['modelo', "talla", "color", "precio"]
+
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "email", "fecha_de_afiliacion"]
+    search_fields = ["nombre", "email", "fecha_de_afiliacion"]
+
+
+class AccesorioAdmin(admin.ModelAdmin):
+    list_display = ['modelo', 'talla', "color", "precio"]
+    search_fields = ['modelo', "talla", "color", "precio"]
+
+class SucursalAdmin(admin.ModelAdmin):
+    list_display = ["num_Sucursal", "nombre_sucursal"]
+    search_fields = ["num_Sucursal", "nombre_sucursal", "direccion"]
+
+
 
 admin.site.register(Zapatos, ZapatoAdmin)
-admin.site.register(Proveedores)
-admin.site.register(Accesorios)
-admin.site.register(Sucursales)
+admin.site.register(Proveedores, ProveedorAdmin)
+admin.site.register(Accesorios, AccesorioAdmin)
+admin.site.register(Sucursales, SucursalAdmin)
 
--- App_zap/models.py
+++ App_zap/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Proveedores(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    email = models.EmailField()
+    fecha_de_afiliacion = models.DateField(null=False, max_length=50)
+
+    def __str__(self):
+        return f'{self.nombre} - {self.fecha_de_afiliacion}'
+
 
 class Zapatos(models.Model):
 
@@ -8,6 +19,7 @@ class Zapatos(models.Model):
     color = models.CharField(max_length=50)
     talla = models.FloatField()
     precio = models.FloatField()
+    proveedor = models.ForeignKey(Proveedores, on_delete=models.CASCADE, default=1)
 
     def __str__(self) -> str:
         return f'{self.modelo} - {self.color} - {self.talla}'
@@ -19,24 +31,25 @@ class Zapatos(models.Model):
         unique_together = ('modelo','talla')
 
 
-
 class Accesorios(models.Model):
 
     modelo = models.CharField(max_length=50)
     color = models.CharField(max_length=50)
     talla = models.FloatField()
     precio = models.FloatField()
+    proveedor = models.ForeignKey(Proveedores, on_delete=models.CASCADE, default=1)
 
-class Proveedores(models.Model):
+    def __str__(self):
+        return f'{self.modelo} - {self.talla} - {self.color} - {self.proveedor}'
 
-    nombre = models.CharField(max_length=50)
-    email = models.EmailField()
-    fecha_de_afiliacion = models.DateField(null=False, max_length=50)
 
 class Sucursales(models.Model):
 
     num_Sucursal = models.IntegerField()
     nombre_sucursal = models.CharField(max_length=50)
     direccion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.num_Sucursal} - {self.nombre_sucursal}'
 
     
